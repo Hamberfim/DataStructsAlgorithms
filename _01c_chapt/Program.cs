d3d82ffb9c92220330fb9f1c4a818b2bbd994af4
Anthony Hamlin <adhamlingithub@gmail.com>
2022-05-20 16:07:44 -0500

    time example

---
+++ _01c_chapt/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace _01c_chapt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your birthdate: ");
+            // read input from user
+            string birthdateString = Console.ReadLine();
+
+            // convert string to date
+            if(!DateTime.TryParseExact(birthdateString, "M/d/yyyy", new CultureInfo("en-US"), DateTimeStyles.None, out DateTime birthDateTime))
+            {
+                birthDateTime = DateTime.Now;
+            }
+
+            Console.WriteLine($"Your birth day is {birthDateTime}");
+
+            Console.WriteLine();  // space in output
+        }
+    }
+}
