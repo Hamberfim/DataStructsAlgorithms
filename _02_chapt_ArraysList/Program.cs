ae57fc7ca027ec67ff188be0a257ffce8e87a920
Anthony Hamlin <adhamlingithub@gmail.com>
2022-05-24 13:20:09 -0500

    lists, arrays,etc.

---
+++ _02_chapt_ArraysList/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02_chapt_ArraysList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();  // space in output
+        }
+    }
+}
