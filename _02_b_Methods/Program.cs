5da124abdb4725e1b90ff2a8257bdfa4eb8dfc4b
Anthony Hamlin <adhamlingithub@gmail.com>
2022-05-25 12:57:43 -0500

    review methods

---
+++ _02_b_Methods/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _02_b_Methods
+{
+
+    class BigApp 
+    { 
+        public void MyPrint()
+        {
+            Console.WriteLine("Hello ");
+        }
+
+        // overloading
+        public void MyPrint(string word)
+        {
+            Console.WriteLine("Hello " + word);
+        }
+
+        public void MyPrint(int num)
+        {
+            Console.WriteLine("Your Big App number is: " + num);
+        }
+
+
+    }
+    
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // call methods
+            BigApp app = new BigApp();
+            app.MyPrint();
+            app.MyPrint("Big App");
+            app.MyPrint(7);
+
+            Console.WriteLine();  // space
+        }
+    }
+}
