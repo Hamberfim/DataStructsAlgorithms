fb212032ad286089602a0ae9a197de13581a5c78
Anthony Hamlin <adhamlingithub@gmail.com>
2022-05-25 10:06:10 -0500

    lists, Arrays

--- _02_chapt_ArraysList/Program.cs
+++ _02_chapt_ArraysList/Program.cs
@@ -15,11 +15,12 @@ namespace _02_chapt_ArraysList
             for (int month = 1; month <= 12; month++)  // loop thru months 1 - 12
             {
                 DateTime firstDay = new DateTime(DateTime.Now.Year, month, 1);
+                
 
                 // cultural info set to english
                 string name = firstDay.ToString("MMMM", CultureInfo.CreateSpecificCulture("en-us"));
 
-                months[month - 1] = name;
+                months[month - 1] = name;  // zero based index so subtract 1
             }
 
             // print out the months
@@ -29,6 +30,26 @@ namespace _02_chapt_ArraysList
             }
 
             Console.WriteLine();  // space in output
+
+            // multi-dimentional arrays
+            int[,] myNumbers = new int[,] 
+                {
+                    { 9, 10 },
+                    {-3, 6 },
+                    { 4, -10}
+            
+                };
+            Console.WriteLine($"1st Line: {myNumbers[0, 0]}, {myNumbers[0, 1]}");
+            Console.WriteLine($"2nd Line: {myNumbers[1, 0]}, {myNumbers[1, 1]}");
+            Console.WriteLine($"3rd Line: {myNumbers[2, 0]}, {myNumbers[2, 1]}");
+            
+            Console.WriteLine();  // space in output
+            
+            int changeFLine = myNumbers[0, 0];
+            Console.WriteLine($"assign changeFLine <-- myNumbers[0, 0] = {changeFLine}");
+            myNumbers[0, 0] = changeFLine +2;
+
+            Console.WriteLine($"1st Line changed: {myNumbers[0, 0]}, {myNumbers[0, 1]}");
         }
     }
 }
